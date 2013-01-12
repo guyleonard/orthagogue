7ecdb92ae1c03a341138fe2aea8ff91bbf2273d4
Ole Kristian Eksethe <oekseth@gmail.com>
2013-01-12 20:00:47 +0100

    some updates

---
+++ blast_filtering/files_for_libs.cmake
@@ -0,0 +1,8 @@
+set(CXXFILES_BLAST_FILTERING
+  blast_filtering.cxx  pipe_ortholog_inparalog.cxx  pipe_bucket.cxx  pipe_merge.cxx  pipe_norm.cxx  pipe_struct.cxx  pipe_write.cxx  build_string.cxx
+   taxon_pair.cxx bucket_norm.cxx bucket_pipe_binary.cxx
+)
+
+set(CXXFILES_BLAST_FILTERING_AS_LIBRARY 
+  blast_filtering.cxx 
+)
