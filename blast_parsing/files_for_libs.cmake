ea72f2a685b93b9c19380203610b4d83e20ffb64
Ole Kristian Eksethe <oekseth@gmail.com>
2013-01-12 20:01:20 +0100

    some updates

---
+++ blast_parsing/files_for_libs.cmake
@@ -0,0 +1,9 @@
+set(CXXFILES_BLAST_PARSING
+string_section.cxx  blast_extractors.cxx    buffer_string.cxx       parse_send.cxx        pipe_parse_merge.cxx  read_file.cxx       taxon_list_t.cxx
+blast_parsing.cxx       buffer_string_list.cxx  parse_send_first.cxx  pipe_parse_parse.cxx   thread_reading.cxx
+parse_read_blocks.cxx   pipe_parse_file.cxx   pre_read_blast.cxx    taxon_data.cxx
+)
+
+set(CXXFILES_BLAST_PARSING_AS_LIBRARY 
+  blast_parsing.cxx 
+)
