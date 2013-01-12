6924d33d1ff77e16ff515c850ed5ae16f4c58795
Ole Kristian Eksethe <oekseth@gmail.com>
2013-01-12 20:00:01 +0100

    some updates

---
+++ terminal_input/files_for_libs.cmake
@@ -0,0 +1,30 @@
+set(CXXFILES_BLAST_TERMINAL
+  cmd_argument.cxx  cmd_list.cxx 
+  )
+
+# Those that not shall be accessable as librries are set here:
+set(CXXFILES_BLAST_TERMINAL_LOCAL )
+
+# set(CXXFILES_BLAST_COMMON
+#   taxa.cxx parse.cxx norm_t.cxx
+#   prot_list.cxx 
+#   algo_overlap.cxx
+#   id_simil_list.cxx
+#   list_file_struct.cxx  file_struct.cxx list_file_parse.cxx   file_parse.cxx
+#   mcl_format.cxx mcl_bunch.cxx protein_relation.cxx # taxon_pair 
+#   protein_vector.cxx  
+  
+#   rel.cxx p_rel.cxx
+#   blast_memory.cxx
+#   )
+
+# # Those that not shall be accessable as librries are set here:
+# set(CXXFILES_BLAST_COMMON_LOCAL 
+# #  prot_list.cxx
+# #  algo_overlap.cxx  id_simil_list.cxx    list_file_struct.cxx 
+# # mcl_format.cxx  protein_relation.cxx  taxa.cxx #taxon_pair.cxx
+#  # file_parse.cxx         
+# #x norm_t.cxx    
+# #  protein_vector.cxx    file_struct.cxx   list_file_parse.cxx  mcl_bunch.cxx   parse.cxx 
+
+# )
